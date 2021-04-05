4431818284f5a1531ab42af5873582edfa5c3d27
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-04-05 00:56:02 -0700

    Create another page with different jQuery methods to use

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,8 +1,26 @@
 $(document).ready(function() {
-  $("p").click(function() {
-    $("#walrus-showing").slideToggle();
-    $("#walrus-hidden").slideToggle();
-    $("#doggy-showing").fadeToggle();
-    $("#doggy-hidden").fadeToggle();
+  $("p#walrus").click(function() {
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
   });
+
+  $("p#doggy").click(function() {
+    $("#doggy-showing").toggle();
+    $("#doggy-hidden").toggle();
+  });
+
+  $('#hide').click(function() {
+    $('p#all-img').hide();
+  });
+  $('#show').click(function() {
+    $('p#all-img').show();
+  })
 });
+
+
+// $("p").click(function() {
+//   $("#walrus-showing").slideToggle();
+//   $("#walrus-hidden").slideToggle();
+//   $("#doggy-showing").fadeToggle();
+//   $("#doggy-hidden").fadeToggle();
+// });
